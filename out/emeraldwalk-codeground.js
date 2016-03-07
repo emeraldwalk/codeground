df5b4bd835a787e20dc17932465dc21ccc45e267
Brian Ingles <github@emeraldwalk.com>
2016-03-06 19:45:52 -0500

    Added a license header.

--- out/emeraldwalk-codeground.js
+++ out/emeraldwalk-codeground.js
@@ -9,6 +9,20 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+/*! *****************************************************************************
+Copyright (c) Emeraldwalk. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */ 
 var Emeraldwalk;
 (function (Emeraldwalk) {
     var Codeground;
@@ -502,4 +516,4 @@ var Emeraldwalk;
         })(Samples = Codeground.Samples || (Codeground.Samples = {}));
     })(Codeground = Emeraldwalk.Codeground || (Emeraldwalk.Codeground = {}));
 })(Emeraldwalk || (Emeraldwalk = {}));
-//# sourceMappingURL=emeraldwalk.code_playground.js.map
+//# sourceMappingURL=emeraldwalk-codeground.js.map
